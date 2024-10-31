81414ce7bc579bbe130c59db9166b66cbf3b384b
Shawn Ibragimov <shah.ibrag@gmail.com>
2024-10-31 06:16:26 -0500

    bootstrap added

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,13 @@
 Rails.application.routes.draw do
-  get "breeds/index"
-  get "breeds/show"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # Sets the breeds index as the homepage and defines the breeds resources
+  root 'breeds#index'  
+  resources :breeds, only: [:index, :show]  # Include :index to define breeds_path
+  
+  # Define a route for the About page
+  get 'pages/about', to: 'pages#about', as: 'pages_about'
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-
-  # Render dynamic PWA files from app/views/pwa/*
-  get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
-  get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-
-  # Defines the root path route ("/")
-  # root "posts#index"
-  root 'breeds#index'  # Sets the breeds index as the homepage
-  resources :breeds, only: [:show] do
+  # Define sub-breeds as a nested resource under breeds
+  resources :breeds do
     resources :sub_breeds, only: [:show]
   end
 end
